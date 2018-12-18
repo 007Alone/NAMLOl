82f977c1099b20704482b900b671829f88e0c324
007Alone <44652106+007Alone@users.noreply.github.com>
2018-12-18 11:13:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('message', message => {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
+  if (message.content.startsWith(adminprefix + 'setx')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
